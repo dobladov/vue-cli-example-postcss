3786ce2cbfde2ea8386ae5b81ddf62aca3c99407
Daniel Doblado <danieldoblado@gmail.com>
2018-12-16 20:22:42 +0100

    Add initial code

---
+++ README.md
@@ -0,0 +1,8 @@
+## Example of vue-cli with postcss nesting working
+
+```
+git clone https://github.com/dobladov/vue-cli-example-postcss.git
+cd vue-cli-example-postcss
+npm run start
+
+```
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1 class="foo">
+      Foobar
+    </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+body {
+  background-color: tomato;
+
+  & .foo {
+    color: purple;
+  }
+}
+</style>
